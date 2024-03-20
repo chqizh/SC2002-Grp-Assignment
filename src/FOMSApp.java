c147b3bc531f68c255e9a393ed5e7ab0cdf9de41
chqizh <109030951+chqizh@users.noreply.github.com>
2024-03-21 01:42:24 +0800

    Update

--- src/FOMSApp.java
+++ src/FOMSApp.java
@@ -1,5 +1,14 @@
+import java.lang.System;
+
 public class FOMSApp{
+    // Declaring ANSI_RESET so that we can reset the color 
+    public static final String ANSI_RESET = "\u001B[0m"; 
+    // Declaring the color 
+    public static final String ANSI_CYAN = "\u001B[36m";
+
     public static void main(String[] args) {
+        System.out.println(ANSI_CYAN + "Menu:" + ANSI_RESET);
+
         return;
     }
 }
