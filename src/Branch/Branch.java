db61d237d307a261279993e42e4bffa3a18d4fc5
boslyn <125525368+boslyn@users.noreply.github.com>
2024-04-05 22:25:00 +0800

    Update Branch.java

--- src/Branch/Branch.java
+++ src/Branch/Branch.java
@@ -26,6 +26,7 @@ public class Branch {
         //TODO
         this.branchManagers = branchManagers;
         numManagers++;
+        //test test test 
     }
 
     public Staff[] getStaffList() {
