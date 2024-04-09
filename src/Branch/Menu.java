12ddfc0d3782bb735d8c05e788ae00c345137cf7
yuehui-tan <tanyuehui288@gmail.com>
2024-04-09 23:39:46 +0800

    Update

--- src/Branch/Menu.java
+++ src/Branch/Menu.java
@@ -1,11 +1,18 @@
 package Branch;
-
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Menu {
     private String branchID;
-    private static MenuItem menu[];
+    //private static MenuItem menu[];
+    private static List<MenuItem> menu;
     private static int numItems;
+
+    public Menu(){
+        this.menu = new ArrayList<>();
+    }
+
 
     public void setBranchID(String branchID) {
         this.branchID = branchID;
@@ -15,19 +22,36 @@ public class Menu {
         return branchID;
     }
 
-    public static void setMenu(MenuItem[] menu) {
-        //TODO
-        Menu.menu = menu;
+    public void printMenuItems() {
+        
+        System.out.println("---------------------------------------------------------------");
+        System.out.printf("| %-10s | %-30s | %-10s |%n", "Item ID", "Name", "Price");
+        System.out.println("---------------------------------------------------------------");
+
+        // Print menu items
+        for (MenuItem item : menu) {
+            System.out.printf("| %-10d | %-30s | $%-9.2f |%n", item.getItemID(), item.getItemName(), item.getPrice());
+        }
+
+        // Print table footer
+        System.out.println("----------------------------------------------------------------");
     }
 
-    public static MenuItem[] getMenu() {
-        return menu;
+    public void addMenuItem(MenuItem menuItem) {
+        menu.add(menuItem);
     }
 
-    public static void printMenu(){
-        //TODO
-        for (int i = 0; i < numItems; i++){
-            System.out.printf("Item: %s, Price: %f", menu[i].getItemName(), menu[i].getPrice() );
+    public void removeMenuItem(int itemId) {
+        int index = -1;
+        for (int i = 0; i < menu.size(); i++) {
+            if (menu.get(i).getItemID() == itemId) {
+                index = i;
+                break;
+            }
+        }
+        // Remove the item if found
+        if (index != -1) {
+            menu.remove(index);
         }
     }
 }
