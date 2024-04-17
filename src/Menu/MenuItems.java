268eff0722edb0c6500e9496e352b1be66e45320
cgtdqykpu <164150728+cgtdqykpu@users.noreply.github.com>
2024-04-18 00:40:18 +0800

    Update MenuItems.java
    Created new constructors and methods to allow for file persistence

--- src/Menu/MenuItems.java
+++ src/Menu/MenuItems.java
@@ -5,25 +5,36 @@ import java.util.*;
 
 public class MenuItems {
 	
-	public String name;
-	public String price;
-	public String branch;
-	public String category;
-	public String availability;
-	public String itemInfo;
-	public int id;
+	public String name=null;
+	public String price=null;
+	public String branch=null;
+	public String category=null;
+	public String availability=null;
+	public String itemInfo=null;
+	public String id = null;
 	
 	public MenuItems() {
 	    System.out.println("Here we create a menu item");
 	    createMenuItem();
 	}
 	
-	public MenuItems(String name, String price, String branch, String category, String availability) {
+	public MenuItems(String id, String name, String price, String branch, String category, String availability) {
+	    this.id = id;
 	    this.name = name;
 	    this.price = price;
 	    this.branch = branch;
 	    this.category = category;
 	    this.availability = availability;
+	    this.itemInfo = String.format(",%s,%s,%s,%s,%s\n", this.name, this.price, this.branch, this.category, this.availability);
+	}
+	
+	public MenuItems(String[] menuItemInfoList) {
+	    this.id = menuItemInfoList[0];
+	    this.name = menuItemInfoList[1];
+	    this.price = menuItemInfoList[2];
+	    this.branch = menuItemInfoList[3];
+	    this.category = menuItemInfoList[4];
+	    this.availability = menuItemInfoList[5];
 	    this.itemInfo = String.format(",%s,%s,%s,%s,%s\n", this.name, this.price, this.branch, this.category, this.availability);
 	}
 	
@@ -48,10 +59,12 @@ public class MenuItems {
 		
 		System.out.println("Enter menu item availability");
 	    	String a = sc.nextLine();
-		this.category = a;
-		
-		sc.close();
+		this.availability = a;
 		
 		this.itemInfo = String.format(",%s,%s,%s,%s,%s\n", this.name, this.price, this.branch, this.category, this.availability);
 	}	
+	
+	public void updateItemInfo() {
+		this.itemInfo = String.format(",%s,%s,%s,%s,%s\n", this.name, this.price, this.branch, this.category, this.availability);
+	}
 }
