7b741e3d1431e33081d9be81f6c26628cfa3ebec
cgtdqykpu <164150728+cgtdqykpu@users.noreply.github.com>
2024-04-21 01:05:38 +0800

    Update MenuDisplay.java

--- src/OldMenu/MenuDisplay.java
+++ src/OldMenu/MenuDisplay.java
@@ -6,16 +6,39 @@ import java.util.*;
 //We will use exception handling when customer makes orders
 //If they select from different branches, we say it is an invalid order
 //"Invalid order. If you would like to order from multiple branches, please make a separate order for each branch."
+//We show all menu from all branches at the start so that customers have more options to choose from
 public class MenuDisplay {
-	File menuFile = new File("Menu.txt");
+	
+	public MenuDisplay() {}
 		
-	public void displayFullMenu(List<MenuItem> menu) {
+	public void displayFullMenu(Menu menu) {
 		System.out.println("This is the current menu:");
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(menuFile));
+			BufferedReader reader = new BufferedReader(new FileReader(menu.menuFile));
 			String line;
 			while ((line = reader.readLine()) != null) {
 				System.out.println(line);
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("End of menu");
+	} 
+	
+	//We can also give customers the option to view just a specific branch's menu if they wish to do so
+	public void displayBranchMenu(Menu menu, String branch) {
+		System.out.println("This is the menu for your branch:");
+		menu.initialiseMenuFromFile();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(menu.menuFile));
+			String line;
+			int lineCount = 0;
+			while ((line = reader.readLine()) != null)  {
+				if (menu.menu[lineCount].branch.equals("b")) {
+					System.out.println(line);
+				}
+				lineCount++;
 			}
 			reader.close();
 		} catch (IOException e) {
